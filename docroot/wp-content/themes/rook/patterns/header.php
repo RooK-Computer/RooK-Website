2f49cece50ef51688220a37e185e27cbc20995d9
Sebastian Srb <sebastian.srb@palasthotel.de>
2026-05-12 22:45:10 +0200

    add r∞k-child-theme

---
+++ docroot/wp-content/themes/rook/patterns/header.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: header
+ * Slug: rook/header
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"align":"full","className":"is-style-section-3","style":{"color":{"background":"#ffea00"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull is-style-section-3 has-background" style="background-color:#ffea00"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"width":100} /-->
+
+<!-- wp:image {"lightbox":{"enabled":false},"width":"150px","sizeSlug":"full","linkDestination":"custom"} -->
+<figure class="wp-block-image size-full is-resized"><a href="/"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/rook_logo_v1-0-0_name_color-scaled.png" alt="" class="" style="width:150px"/></a></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"overlayBackgroundColor":"base","overlayTextColor":"contrast","className":"with-bar","fontSize":"large","layout":{"type":"flex","justifyContent":"right","flexWrap":"wrap"}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
